050adde4e5f72b46842504227ae31d63c5cdf270
JjungminLee <julie0964@naver.com>
2024-01-25 10:17:17 +0900

    240125

---
+++ 2024_01/Boj_14889.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int arr[21][21];
+int minAns = INT_MAX;
+bool visits[21];
+
+void dfs(int idx,int depth){
+    vector<int>start={};
+    vector<int>link={};
+    int startCnt=0;
+    int linkCnt=0;
+    if(depth==N/2){
+        for(int i=0;i<N;i++){
+            if(visits[i]){
+                start.push_back(i);
+            }else{
+                link.push_back(i);
+            }
+        }
+      
+        for(int i=0;i<start.size();i++){
+          for(int j=0;j<start.size();j++){
+            startCnt+=arr[start[i]][start[j]];
+          }
+        }
+         for(int i=0;i<link.size();i++){
+          for(int j=0;j<link.size();j++){
+            linkCnt+=arr[link[i]][link[j]];
+          }
+        }
+
+        minAns=min(minAns,abs(startCnt-linkCnt));
+      
+        return;
+    }else{
+        for(int i=idx;i<N;i++){
+            if(!visits[i]){
+                visits[i]=true;
+                // 0 1 2 , 0 2 3 이런식으로 오름차순 방향으로 가지
+                // 3 0 1 이런건 생각해주지 않는거! -> 시간초과!
+                dfs(i,depth+1);
+                visits[i]=false;
+            }
+        }
+    }
+}
+int main(){
+    cin>>N;
+    vector<int> start;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cin>>arr[i][j];
+        }
+    }
+    dfs(0,0);
+
+    cout<<minAns<<endl;
+
+}
